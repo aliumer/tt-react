d04363777b098605feb89132d1e3caf443e00ce2
Muhammad Ali Umer <ilaumer@gmail.com>
2026-06-21 18:53:53 +0100

    added sound for correct and incorrect key press.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Route, Switch } from "react-router-dom";
 import Header from './Header';
-// import Typing from './typing';
 import TypingSmooth from './tt.smoth'
 
 import Mouse from './mouse';
--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,10 @@ module.exports = {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.mp3$/,
+                type: 'asset/resource'
+            }
         ]
     },
     devServer: {
